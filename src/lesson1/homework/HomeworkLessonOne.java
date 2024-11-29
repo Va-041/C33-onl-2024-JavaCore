3949193dfc43122047d3b496b48e2b51785aecde
Vadim <99527038+Va-041@users.noreply.github.com>
2024-11-29 23:21:38 +0300

    Update homework lesson one, lesson two

--- src/lesson1/homework/HomeworkLessonOne.java
+++ src/lesson1/homework/HomeworkLessonOne.java
@@ -1,5 +1,117 @@
-public class Main {
+package lesson1.homework;
+
+/**
+ *<a>This is my first homework assignment in the Java developer courses from TeachMeSkills, which I am doing
+ * <p>I hope that I will be able to successfully carry them out in the future
+ * </p>
+ *</a>
+ * @author Soroka Vadim, C33-onl2024
+ */
+
+class HomeworkLessonOne {               //here I follow the first rule from the Java Code Conventions... class = noun, UpperCamel
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // main is entry-point
+
+        //Method call
+        getBasicInformationOnJava();
+        setBasicVariables();
+        arithmeticOperators();
+
+    }
+    public static void getBasicInformationOnJava() { // here I follow the fifth rule JCC... method = verb,LowerCamel
+        System.out.println("\nThis is a method with basic information about the Java language, called using next line");
+        System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
+        System.out.println();
+    }
+
+    public static void setBasicVariables() {
+        System.out.print("This is a method with basic information about variables, called without using next line\n");
+        System.out.println();
+
+        System.out.println("= = = = Integer = = = =");
+        byte byteValue = 11;
+        System.out.println("byteValue = " + byteValue + " -> this is a primitive 8-bit integer byte-type variable " +
+                "with a value range of -128 ~ 127");
+
+        short shortValue = 5533;
+        System.out.println("shortValue = " + shortValue + " -> this is a primitive 16-bit integer short-type variable" +
+                " with a value range of -32768 ~ 32767");
+        System.out.println("These types are practically not used at the moment, only in exceptional cases\n");
+        // Byte and short used for memory saving, no more actual unless you have very little memory
+        // If you use byte and short in arithmetic operations and/or pass it as parameters they will by default
+        // turn to integer
+
+        char charValue = 'V';
+        System.out.println("charValue = " + charValue + " -> this is a primitive 16-bit integer char-type integer " +
+                "variable which is officially it is a integer value, but factually it is a character");
+
+        char charASCIIValue = 90;
+        System.out.println("charASCIIValue = " + charASCIIValue + " -> this is the same integer char-type variable, " +
+                "but written using the character code from the ASCII-table in decimal representation");
+
+        int intValue = 225133;
+        System.out.println("intValue = " + intValue + " -> this is a primitive 32-bit integer int-type variable with " +
+                "a value range of -2147483648 ~ 2147483647");
+
+        long longValue = 18_05_2004L;
+        System.out.println("longValue = " + longValue + " -> this is a primitive 64-bit integer long-type variable " +
+                "with a range of -9223372036854775808 ~ 9223372036854775808 and a special suffix L");
+        System.out.println("\nUnderscores are used to separate large numbers for easier readability (to see how many " +
+                "digits are in a number)\n" + "\n" + "Special suffixes for long (for example, 39832L), " +
+                "float (for example, 2.4) and double (for example, -7.832D) are needed so that the compiler does " +
+                "not try to lower the data type (for example, when writing long longValue = 2; the compiler will " +
+                "think why allocate long memory for an ordinary two, if it has enough int, and He will count himself");
+
+
+        // float PI = 3.1415F; -> f, F
+        // double PI = 3.1415; -> if arithmetic or parameters needed
+        System.out.println("\n\n= = = = Floating numbers = = = =");
+        float floatValue = 18.999F; // . 7 numbers
+        System.out.println("floatValue = " + floatValue + " -> this is a primitive 32-bit floating point variable " +
+                "of the float-type with a range of values 1.4e-45f ~ 3.4e+38f ( . 7 numbers)");
+
+        double doubleValue = 555.554433; // . 15 numbers, by default
+        System.out.println("doubleValue = " + doubleValue + " -> this is a primitive 64-bit floating point variable " +
+                "of the double-type with a range of values 4.9e-324f ~ 1.7e+308 ( . 15 numbers); default for " +
+                "arithmetic operations and value transfer");
+
+        // C/C++ !!! in Java boolean means boolean, only TRUE or FALSE
+        // C/C++ 0 == false, != 0 true
+        System.out.println("\n\n= = = = Logical variable = = = =");
+        boolean isAlive = true;
+        boolean isMarried = false;
+        System.out.println(isMarried);
+        System.out.println(isAlive);
+
+        System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
+    }
+
+    public static void arithmeticOperators() {
+        System.out.println("\nThis is a method with basic arithmetic operators\n");
+
+        int height = 182;
+        double weight = 65.4;
+
+        int getSumHeightAddition = height + (height / 10);
+        System.out.println("getSumHeight = " + getSumHeightAddition);
+
+        double getSumWeightDivided =  weight / 2 + getSumHeightAddition;
+        System.out.println("getSumWeight = " + getSumWeightDivided);
+
+        float getSumWeightSubtraction = (float) (weight - 44.3456f);    // casting...?
+        System.out.println("getSumWeightSubtraction = " + getSumWeightSubtraction);
+
+        int getMultipleHeightModulus = 24;
+        long multipleFactor = 5;
+        getMultipleHeightModulus = (int) (height * multipleFactor) % getMultipleHeightModulus;
+        System.out.println("getMultipleHeightModulus = " + getMultipleHeightModulus);
+
+        int valueIncrement = 99;
+        valueIncrement++;
+        System.out.println("valueIncrement = " + valueIncrement);
+
+        int valueDecrement = 112;
+        valueDecrement--;
+        System.out.println("valueDecrement = " + valueDecrement);
     }
 }
--- src/lesson2/homework/HomeworkLessonTwo.java
+++ src/lesson2/homework/HomeworkLessonTwo.java
@@ -1,4 +1,358 @@
 package lesson2.homework;
 
-public class HomeworkLessonTwo {
+import java.util.Locale;
+import java.util.Scanner;
+
+class HomeworkLessonTwo {
+    public static void main(String[] args) {
+        getResultTaskOne();
+    }
+
+    private static int currentTask = 1;
+
+    public static void getResultTaskOne() {
+        System.out.println("\n= = = = = = = = = = = Task №1 = = = = = = = = = = =\n");
+
+        int valueB = getValidInt("Please, enter value B: ", 1);
+        int valueC = getValidInt("Please, enter value C: ", 1);
+        int valueA = 4 * (valueB + valueC - 1) / 2;
+        System.out.println("The result of the calculation a = 4 * (b + c - 1)/2 = " + valueA);
+
+        getChoiceAfterResult();
+    }
+
+    public static void getResultTaskTwo() {
+        System.out.println("= = = = = = = = = = = Task №2 = = = = = = = = = = =\n");
+
+        int valueTwoDigitN = getValidInt("Please, enter a two-digit number: ", 2);
+        int firstDigit;
+        int secondDigit;
+
+        if (valueTwoDigitN < 0){
+            firstDigit = -valueTwoDigitN / 10;
+            secondDigit = -valueTwoDigitN % 10;
+        } else {
+            firstDigit = valueTwoDigitN / 10;
+            secondDigit = valueTwoDigitN % 10;
+        }
+
+        int sumOfDigits = firstDigit + secondDigit;
+        System.out.println("The sum of the digits of the number N: " + firstDigit + " + " + secondDigit + " = "
+                + sumOfDigits );
+
+        getChoiceAfterResult();
+    }
+
+    public static void getResultTaskThree() {
+        System.out.println("= = = = = = = = = = = Task №3 = = = = = = = = = = =\n");
+
+        int valueThreeDigitN = getValidInt("Please, enter a three-digit number: ", 3);
+        int firstDigit;
+        int secondDigit;
+        int thirdDigit;
+
+        if (valueThreeDigitN < 0){
+            firstDigit = -valueThreeDigitN / 100;
+            secondDigit = -(valueThreeDigitN / 10) % 10;
+            thirdDigit = -valueThreeDigitN % 10;
+        } else {
+            firstDigit = valueThreeDigitN / 100;
+            secondDigit = (valueThreeDigitN / 10) % 10;
+            thirdDigit = valueThreeDigitN % 10;
+        }
+
+        int sumOfDigits = firstDigit + secondDigit + thirdDigit;
+        System.out.println("The sum of the digits of the number N: " + firstDigit + " + " + secondDigit + " + " +
+                thirdDigit + " = " + sumOfDigits);
+
+        getChoiceAfterResult();
+    }
+
+    public static void getResultTaskFour() {
+        System.out.println("= = = = = = = = = = = Task №4 = = = = = = = = = = =\n");
+
+        double doubleValueN = getValidDouble("\nPlease, enter a real number with a non-zero " +
+                "fractional part: ", -999, 999);
+
+        int resultRounded;
+        if (doubleValueN >= 0) {
+            resultRounded = (int)(doubleValueN + 0.5);
+        } else {
+            resultRounded = (int)(doubleValueN - 0.5);
+        }
+        System.out.println("The result of rounding: " + resultRounded);
+        //     String resultRounding = String.format("%.0f",valueN);
+        //     System.out.print("The result of rounding: " + resultRounding);
+
+        getChoiceAfterResult();
+    }
+
+    public static void getResultTaskFive() {
+        System.out.println("= = = = = = = = = = = Task №5 = = = = = = = = = = =\n");
+
+        int valueQ = getValidInt("Please, enter value Q: ", 1);
+        int valueW = getValidInt("Please, enter value W: ", 1);
+
+        if (valueW == 0) {
+            System.out.println("Error: Division by zero is not allowed.");
+        } else {
+            int resultDivision = valueQ / valueW;
+            int resultModulus = valueQ % valueW;
+
+            if (valueQ < 0 && valueW < 0) {
+                System.out.println("Result: " + valueQ + " / " + "(" + valueW + ")" + " = " + resultDivision + " and " +
+                        resultModulus + " in modulus");
+
+            } else if (valueQ > 0 && valueW < 0) {
+                System.out.println("Result: " + valueQ + " / " + "(" + valueW + ")" + " = " + resultDivision + " and " +
+                        resultModulus + " in modulus");
+
+            } else {
+                System.out.println("Result: " + valueQ + " / " + valueW + " = " + resultDivision + " and " +
+                        resultModulus + " in modulus");
+            }
+        }
+
+        getChoiceAfterResult();
+    }
+
+    public static void getResultTaskWithAsterisk() {
+        System.out.println("= = = = = = = = Task with asterisk = = = = = = = = =\n");
+
+        int valueA = getValidInt("Please, enter value A: ", 1);
+        int valueB = getValidInt("Please, enter value B: ", 1);
+
+        // Swap values using XOR
+        valueA ^= valueB;
+        valueB ^= valueA;
+        valueA ^= valueB;
+
+        System.out.println("\nResult:\nValue A = " + valueA + "\nValue B = " + valueB);
+
+        getChoiceAfterResult();
+    }
+
+
+
+
+    public static Scanner getScannerInput() {
+        return new Scanner(System.in);
+    }
+
+    public static int getValidInt(String outputString, int digitCount) {
+
+        Scanner inputValue = getScannerInput();
+
+        int valueInt = 0;
+        boolean isValid = false;
+
+        while (!isValid) {
+            System.out.print(outputString);
+            if (inputValue.hasNextInt()) {
+                valueInt = inputValue.nextInt();
+
+                // Проверка в зависимости от количества разрядов
+                if (digitCount == 1) {
+                    isValid = true; // Для однозначного числа просто разрешаем любой ввод
+                } else if (digitCount == 2) {
+                    if ((valueInt >= 10 && valueInt <= 99) || (valueInt <= -10 && valueInt >= -99)) {
+                        isValid = true;
+                    } else {
+                        System.out.println("The entered number is not two-digit, repeat the input!");
+                    }
+                } else if (digitCount == 3) {
+                    if ((valueInt >= 100 && valueInt <= 999) || (valueInt <= -100 && valueInt >= -999)) {
+                        isValid = true;
+                    } else {
+                        System.out.println("The entered number is not three-digit, repeat the input!");
+                    }
+                } else {
+                    System.out.println("Invalid digit count specification.");
+                }
+            } else {
+                System.out.println("The entered value is not an integer, repeat the input!");
+                inputValue.next(); // Очистка некорректного ввода
+            }
+        }
+        return valueInt;
+    }
+
+    public static double getValidDouble(String outputString, double min, double max) {
+
+        Scanner inputDoubleValue = getScannerInput();
+        inputDoubleValue.useLocale(Locale.ENGLISH);
+
+        double valueDoubleN = 0.0;
+        boolean isValid = false;
+
+        while (!isValid) {
+            System.out.print(outputString);
+
+            if (inputDoubleValue.hasNextDouble()) {
+                valueDoubleN = inputDoubleValue.nextDouble();
+
+                // Проверка диапазона
+                if (valueDoubleN >= min && valueDoubleN <= max) {
+                    // Проверка наличия ненулевой дробной части
+                    if (valueDoubleN % 1 != 0) {
+                        isValid = true;
+                    } else {
+                        System.out.println("The number is an integer, please enter a number with a non-zero " +
+                                "fractional part.");
+                    }
+                } else {
+                    System.out.println("The entered number is out of range (" + min + " to " + max + "). " +
+                            "Please repeat the input!");
+                }
+            } else {
+                System.out.println("The entered value is not a valid double, please repeat the input!");
+                inputDoubleValue.next(); // Очистка некорректного ввода
+            }
+        }
+
+        return valueDoubleN;
+    }
+
+    public static void getChoiceAfterResult() {
+        boolean validChoice = false;
+        Scanner inputChoice = new Scanner(System.in);
+
+        while (!validChoice) {
+            System.out.print("\nSelect an action:");
+
+            int optionNumber = 1; // Переменная для нумерации меню
+
+            System.out.print("\n" + optionNumber++ + ". Repeat"); // 1. Repeat
+
+            if (currentTask < 6) {
+                System.out.print("\n" + optionNumber++ + ". To the next task"); // 2. To the next task
+            }
+
+            if (currentTask > 1) {
+                System.out.print("\n" + optionNumber++ + ". Go back to the previous task"); // 3. Go back to the previous task
+            }
+
+            System.out.print("\n" + optionNumber + ". Exit"); // 4. Exit
+
+            System.out.print("\n\nYour choice: ");
+
+            if (inputChoice.hasNextInt()) {
+                int choiceAction = inputChoice.nextInt();
+
+                if (choiceAction == 1) {
+                    // Повтор текущей задачи
+                    switch (currentTask) {
+                        case 1:
+                            System.out.println("\n- - - - - - - - - - - - - - - - - - - - - - - - - -\n\n");
+                            getResultTaskOne();
+                            break;
+                        case 2:
+                            System.out.println("\n- - - - - - - - - - - - - - - - - - - - - - - - - -\n\n");
+                            getResultTaskTwo();
+                            break;
+                        case 3:
+                            System.out.println("\n- - - - - - - - - - - - - - - - - - - - - - - - - -\n\n");
+                            getResultTaskThree();
+                            break;
+                        case 4:
+                            System.out.println("\n- - - - - - - - - - - - - - - - - - - - - - - - - -\n\n");
+                            getResultTaskFour();
+                            break;
+                        case 5:
+                            System.out.println("\n- - - - - - - - - - - - - - - - - - - - - - - - - -\n\n");
+                            getResultTaskFive();
+                            break;
+                        case 6:
+                            System.out.println("\n- - - - - - - - - - - - - - - - - - - - - - - - - -\n\n");
+                            getResultTaskWithAsterisk();
+                            break;
+                    }
+                    validChoice = true;
+
+                } else if (choiceAction == 2 && currentTask < 6) {
+                    // Переход к следующей задаче
+                    currentTask++;
+                    switch (currentTask) {
+                        case 2:
+                            System.out.println("\n- - - - - - - - - - - - - - - - - - - - - - - - - -\n\n");
+                            getResultTaskTwo();
+                            break;
+                        case 3:
+                            System.out.println("\n- - - - - - - - - - - - - - - - - - - - - - - - - -\n\n");
+                            getResultTaskThree();
+                            break;
+                        case 4:
+                            System.out.println("\n- - - - - - - - - - - - - - - - - - - - - - - - - -\n\n");
+                            getResultTaskFour();
+                            break;
+                        case 5:
+                            System.out.println("\n- - - - - - - - - - - - - - - - - - - - - - - - - -\n\n");
+                            getResultTaskFive();
+                            break;
+                        case 6:
+                            System.out.println("\n- - - - - - - - - - - - - - - - - - - - - - - - - -\n\n");
+                            getResultTaskWithAsterisk();
+                            break;
+                    }
+                    validChoice = true;
+
+                } else if (currentTask == 6) {
+                    // Специальная логика для задачи с астериском
+                    if (choiceAction == 2) {
+                        // Возврат к предыдущей задаче
+                        currentTask--;
+                        System.out.println("\n- - - - - - - - - - - - - - - - - - - - - - - - - -\n\n");
+                        getResultTaskFive();
+                        validChoice = true;
+                    } else if (choiceAction == 3) {
+                        // Выход из программы
+                        System.out.println("Exiting...");
+                        inputChoice.close();
+                        System.exit(0);
+                    } else {
+                        System.out.println("\nChoose one of the available actions!");
+                    }
+
+                } else if (choiceAction == 3 && currentTask > 1) {
+                    // Возврат к предыдущей задаче
+                    currentTask--;
+                    switch (currentTask) {
+                        case 1:
+                            System.out.println("\n- - - - - - - - - - - - - - - - - - - - - - - - - -\n\n");
+                            getResultTaskOne();
+                            break;
+                        case 2:
+                            System.out.println("\n- - - - - - - - - - - - - - - - - - - - - - - - - -\n\n");
+                            getResultTaskTwo();
+                            break;
+                        case 3:
+                            System.out.println("\n- - - - - - - - - - - - - - - - - - - - - - - - - -\n\n");
+                            getResultTaskThree();
+                            break;
+                        case 4:
+                            System.out.println("\n- - - - - - - - - - - - - - - - - - - - - - - - - -\n\n");
+                            getResultTaskFour();
+                            break;
+                        case 5:
+                            System.out.println("\n- - - - - - - - - - - - - - - - - - - - - - - - - -\n\n");
+                            getResultTaskFive();
+                            break;
+                    }
+                    validChoice = true;
+
+                } else if (choiceAction == optionNumber && currentTask > 0) {
+                    // Выход из программы
+                    System.out.println("\n- - - - - - - - - - - - - - - - - - - - - - - - - -\n\n");
+                    System.out.println("\n\nExiting...");
+                    inputChoice.close();
+                    System.exit(0);
+                } else {
+                    System.out.println("\nChoose one of the available actions!");
+                }
+            } else {
+                System.out.println("Invalid input, please enter a number.");
+                inputChoice.next(); // очистка неправильного ввода
+            }
+        }
+    }
 }
